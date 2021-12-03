468da0c1d6cdc3ab88dee10c71296c2110bb1c29
JoseGuadalupeM <jose.martinez@ies-systems.com>
2021-12-03 01:56:04 -0600

    inicio del proyecto de ToDo usando Redux

---
+++ src/app/todos/todo.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodoAddComponent } from './components/todo-add/todo-add.component';
+import { TodoFooterComponent } from './components/todo-footer/todo-footer.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoPageComponent } from './todo-page/todo-page.component';
+
+const COMPONENTS = [
+  TodoAddComponent,
+  TodoFooterComponent,
+  TodoItemComponent,
+  TodoListComponent,
+  TodoPageComponent
+]
+
+@NgModule({
+  declarations: [...COMPONENTS],
+  imports: [
+    CommonModule
+  ]
+})
+export class TodoModule { }
